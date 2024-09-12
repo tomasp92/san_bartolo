66a7aac9b38ed9cc18a76ba7879d2053284279ab
tomasp92 <tomas@atexto.com>
2024-09-12 08:59:43 -0300

    change app to use vercel

---
+++ api/send-emails.js
@@ -0,0 +1,32 @@
+const multer = require('multer')
+const { sendEmails } = require('../services')
+require('dotenv').config()
+
+const upload = multer({ dest: '/tmp/uploads/' })
+
+module.exports = (req, res) => {
+  upload.single('excelFile')(req, res, async (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Error al subir el archivo' })
+    }
+    
+    const additionalMessage = req.body.additionalMessage || ''
+    const pass = req.body.pass || ''
+    
+    if (pass !== process.env.PASSWORD) {
+      return res.status(401).json({
+        message: 'Contraseña incorrecta'
+      })
+    }
+    
+    try {
+      const { sentCount, emails } = await sendEmails({ file: req.file.path, additionalMessage })
+      return res.status(200).json({
+        message: `${sentCount} correos enviados exitosamente. Los siguientes emails recibieron la rendición: ${emails}`
+      })
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ message: 'Error al enviar correos' })
+    }
+  })
+}
